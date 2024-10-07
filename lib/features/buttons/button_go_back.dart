a0cc3e3ad0bda43984193b29b3b3ece19a1c3de4
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-10-07 15:11:32 +0200

    implemented universal button "GoBack"

---
+++ lib/features/buttons/button_go_back.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class ButtonGoBack extends StatelessWidget {
+  const ButtonGoBack({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+      style: ElevatedButton.styleFrom(backgroundColor: Colors.teal[600]),
+      child: const Text(
+        'Go Back',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
