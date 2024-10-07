e3ea06162a29992012c0d766df06a362731a7ac4
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-10-07 13:45:51 +0200

    ButtonRemoveFromFavorites removed to "features/favorites"

--- lib/features/favorites/favorites_details_screen.dart
+++ lib/features/favorites/favorites_details_screen.dart
@@ -1,3 +1,4 @@
+import 'package:aa_travel_planner/features/favorites/button_remove_from_favorites.dart';
 import 'package:aa_travel_planner/features/favorites/favorites_repository.dart';
 import 'package:aa_travel_planner/main.dart';
 import 'package:flutter/material.dart';
@@ -76,17 +77,7 @@ class FavoritesDetailsScreen extends StatelessWidget {
                     fontSize: 16, height: 1.5, color: Colors.grey[700]),
               ),
               const SizedBox(height: 24),
-              ElevatedButton(
-                onPressed: () {
-                  favoritesRepository.removeFavorite(destination);
-                  Navigator.pop(context);
-                },
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                child: const Text(
-                  'Remove from Favorites',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
+              ButtonRemoveFromFavorites(favoritesRepository: favoritesRepository, destination: destination),
             ],
           ),
         ),
