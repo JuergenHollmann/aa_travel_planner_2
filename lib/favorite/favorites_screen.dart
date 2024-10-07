52456ead23ae88f9a90abeca078bf06e88351920
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-10-07 12:36:11 +0200

    _FavoritesScreenState copied to folder "favorite"

---
+++ lib/favorite/favorites_screen.dart
@@ -0,0 +1,33 @@
+import 'package:aa_travel_planner/favorite/favorites_repository.dart';
+import 'package:flutter/material.dart';
+
+class FavoritesScreen extends StatefulWidget {
+  final FavoritesRepository favoritesRepository;
+
+  const FavoritesScreen({super.key, required this.favoritesRepository});
+
+  @override
+  State<FavoritesScreen> createState() => _FavoritesScreenState();
+}
+
+// importieraus main.dart
+class _FavoritesScreenState extends State<FavoritesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    List<Destination> favorites = widget.favoritesRepository.getFavorites();
+
+    return ListView.builder(
+      itemCount: favorites.length,
+      itemBuilder: (context, index) {
+        final destination = favorites[index];
+        return FavoriteCard(
+          destination: destination,
+          favoritesRepository: widget.favoritesRepository,
+          onRemove: () {
+            setState(() {});
+          },
+        );
+      },
+    );
+  }
+}
